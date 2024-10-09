e5e1ec9948781f510f4c4b6f74ba2d2a3c67a359
unknown <keatingcheng@gmail.com>
2024-10-10 02:01:32 +0800

    change config.js

--- webpack.config.js
+++ webpack.config.js
@@ -31,18 +31,18 @@
 const path = require("path");
 
 module.exports = {
-  mode: "development", // 或者 'production'
-  entry: "./src/index.jsx", // 入口文件
-  target: "node", // 指定为 Node.js 环境
+  mode: "development",
+  entry: "./src/index.jsx",
+  target: "node",
   output: {
-    filename: "App.js", // 输出文件名
-    path: path.resolve(__dirname, "public"), // 输出目录
+    filename: "App.js",
+    path: path.resolve(__dirname, "public"),
   },
   module: {
     rules: [
-      // 如果使用 TypeScript，增加以下规则
+
       {
-        test: /\.jsx?$/, // 匹配 .js 或 .jsx 文件
+        test: /\.jsx?$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
@@ -58,6 +58,6 @@ module.exports = {
     ],
   },
   resolve: {
-    extensions: [".js", ".ts"], // 解析的文件扩展名
+    extensions: [".js", ".ts"],
   },
 };
